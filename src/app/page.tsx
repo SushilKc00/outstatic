db55c8b7fc7f719d9e2ab62321f24847afaa2bcd
Hem Bhadur <sushilkc2611@gmail.com>
2024-05-11 11:09:21 +0530

    chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
-export default function Home() {
+import { getDocuments } from "outstatic/server";
+
+export default async function Home() {
+  const posts = await getData();
+  console.log(posts);
   return (
     <main>
       <h2>Outstatic</h2>
     </main>
   );
+}
+async function getData() {
+  const posts = getDocuments("hero", ["title"]);
+
+  return posts;
 }
